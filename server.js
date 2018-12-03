16115683606b124904e71b21c8ec53fb690ca2d7
StevenJBurns <stevenjburns@msn.com>
2018-12-03 00:09:55 -0700

    Installed chalk via NPM and created scripts for node and nodemon startup of the server;  more basic boilerplate added to server.js to get the server running -- and it does but only listens -- needs routing now

--- server.js
+++ server.js
@@ -1,12 +1,20 @@
+/* Node Dependencies */
+const path = require("path");
 const http = require("http");
 
-
+/* External Dependencies */
+const chalk = require("chalk");
 const express = require("express");
 
 
+/* Connect .env file values to Node process.env */
+require("dotenv").config();
+
+/* Clear the console and create the Express server */
 const server = new express();
+console.clear();
 
 
-
-
-
+server.listen(process.env.PORT || 8000, () => {
+  console.log(chalk.bgWhite.black(`  Express Server Started on Port ${process.env.PORT}  `))
+});
