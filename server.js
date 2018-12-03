d476ba68a4db8036a05cd2ff4925c70cac290367
StevenJBurns <stevenjburns@msn.com>
2018-12-03 00:15:39 -0700

    Set up middleware in server.js to use morgan for logging requests and the revived native Express body parsing

--- server.js
+++ server.js
@@ -14,6 +14,11 @@ require("dotenv").config();
 const server = new express();
 console.clear();
 
+/* Morgan Middleware for logging */
+server.use(logger("dev"));
+
+/* Express 4.16 now has native middleware for req.body without body-parser */
+server.use(express.json());
 
 server.listen(process.env.PORT || 8000, () => {
   console.log(chalk.bgWhite.black(`  Express Server Started on Port ${process.env.PORT}  `))
