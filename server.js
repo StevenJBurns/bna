ead7f6e526db9c1e3891482d5c5fb9693eff0df2
StevenJBurns <stevenjburns@msn.com>
2018-12-06 21:21:52 -0700

    Updated dotenv and mongoose via NPM

--- server.js
+++ server.js
@@ -26,11 +26,23 @@ server.use(logger("dev"));
 /* Express 4.16 now has native middleware for req.body without body-parser */
 server.use(express.json());
 
+/* routing for root requests */
+server.use("/", express.static(path.join(__dirname, "client")));
+
 /* Routers to catch /user and /game routes */
 server.use("/user", routerUser);
 server.use("/game", routerGame);
 
+/* Catch-All middleware routing that serves up React client for non-API URLs vs a 404 error */
+server.get("*", (req, res) => res.sendFile(path.join(__dirname, "client", "index.html")));
 
+/* Express catch-all error handler */
+server.use((err, req, res, next) => {
+  console.error("catch-all: ", err);
+  res.json({"error": err});
+});
+
+/* Start the Express server */ 
 server.listen(process.env.PORT || 8000, () => {
   console.log(chalk.bgWhite.black(`  Express Server Started on Port ${process.env.PORT}  `))
 });
